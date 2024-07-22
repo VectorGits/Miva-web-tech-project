073b26f989e118416b2e46c4c5524cb3db96533a
VectorGits <davidoluremi0@gmail.com>
2024-07-22 13:29:08 +0100

    chore: Add CSS styles for about page, trustee page, and shop page

---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+	var navLinks = document.getElementById('nav-links');
+	if (navLinks.style.display === 'flex' || navLinks.style.display === 'grid') {
+		navLinks.style.display = 'none';
+	} else {
+		navLinks.style.display = 'flex';
+	}
+});
+
+// SLIDER
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Ensure the initial slide is visible
+document.addEventListener("DOMContentLoaded", function() {
+  showSlides(slideIndex);
+});
